49f196d8fcba5eadaa61878edb06597c7a84660f
wlassits <wlagyorgy@gmail.com>
2023-09-22 01:21:17 +0200

    Render the App titles from data.csv

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas
+
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
@@ -20,3 +22,14 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit,
  ullamco laboris nisi ut aliquip ex ea commodo consequat
 """
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
