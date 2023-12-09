c47f50ceba447d19f9d64bb55dd4abe5e0e13e11
wlassits <wlagyorgy@gmail.com>
2023-12-09 15:28:32 +0100

    Day 28: Add links, specify language, limit news, add subject, control category

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,14 +1,18 @@
 import streamlit as st
 from send_email import send_email
+import pandas
 
 st.header("Contact Me")
+df = pandas.read_csv("topics.csv")
 
 with st.form(key="email_form"):
     user_email = st.text_input("Your email address")
+    select_topic = st.selectbox('What topic do you want to discuss?',
+                                df["topic"])
     raw_message = st.text_area("Your message")
     message = f"""
     Subject : Nem user email from { user_email}
-    
+    Topic: {select_topic}
     From: {user_email}
     {raw_message}
     """
